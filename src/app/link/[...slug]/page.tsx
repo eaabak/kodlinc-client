c598084c20f9e97c2ec1ffb52d1a3a2f39231eed
Alan <Alan47m@gmail.com>
2024-04-09 22:40:18 +0300

    connceting api service

--- src/app/link/[...slug]/page.tsx
+++ src/app/link/[...slug]/page.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/footer";
 import { detectLanguage } from "@/utils/detectLanguage";
 import { useTheme } from "next-themes";
 import Aside from "@/components/aside";
+import axios from "axios";
 
 function decodeBase64(base64String: string): string {
   const buffer = Buffer.from(base64String, "base64");
@@ -13,21 +14,42 @@ function decodeBase64(base64String: string): string {
 }
 
 const EditorPage: React.FC = (params: any) => {
-  const monaco = useMonaco();
   const [base64String, setBase64String] = useState(params?.params?.slug[0]);
   const { resolvedTheme } = useTheme();
+  const [data, setData] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  // useEffect(() => {
-  //   if (monaco) {
-  //     monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
-  //       noSemanticValidation: true,
-  //       noSyntaxValidation: true,
-  //     });
-  //   }
-  // }, [monaco]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://kodlinc.metafron.com/getData/ec2e5b7f-e103-4c97-a52e-8e31318c133f",
+          {
+            method: "GET",
+            mode: "no-cors", // CORS desteği ekleniyor
+            headers: {
+              "Content-Type": "application/json",
+              // İsteğe ekstra başlıklar eklemek isterseniz buraya ekleyebilirsiniz
+            },
+          }
+        );
+        console.log('response', response)
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const jsonData = await response.text(); // veya response.json() kullanabilirsiniz
+        setData(jsonData);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setIsLoading(false);
+      }
+    };
 
+    fetchData();
+  }, []);
   const code: string = decodeBase64(base64String);
-
+  console.log("data", data);
 
   return (
     <>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,12 +2,17 @@
 
 import React from "react";
 import ThemeSwitch from "./themeSwitch";
+import Logo from "./logo";
 
 const Header = () => {
   return (
     <header
       className={`border-b dark:bg-[rgb(51,51,51)] dark:border-gray-800 bg-white border-[#eee] h-[35px] flex items-center justify-end`}
     >
+      {/* <div className="ml-2">
+        <Logo width={24} height={24} />
+      </div> */}
+
       <div className="mr-2">
         <ThemeSwitch />
       </div>
