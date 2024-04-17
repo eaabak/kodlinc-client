87495c7a65de8943cdf55e62ab2b4972c04a628d
Alan <Alan47m@gmail.com>
2024-04-18 00:19:05 +0300

    feat(KL-1): realtime coding

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,16 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <Router>
-        <div>
-          <Header />
-        </div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/link" element={<Home />} />
-          <Route path="/link/:slug" element={<EditorPage />} />
-          <Route path="*" element={<NotFound text="Page not found"/>} /> 
-        </Routes>
-      </Router>
-    </html>
+    <Router>
+      <div>
+        <Header />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/link" element={<Home />} />
+        <Route path="/link/:slug" element={<EditorPage />} />
+        <Route path="*" element={<NotFound text="Page not found" />} />
+      </Routes>
+    </Router>
   );
 }
--- src/Editor.tsx
+++ src/Editor.tsx
@@ -4,10 +4,14 @@ import { detectLanguage } from "./utils/detectLanguage";
 import Footer from "./components/footer";
 import Aside from "./components/aside";
 import { useTheme } from "./ThemeProvider";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import Loading from "./components/loading";
 import NotFound from "./NotFound";
+import * as Y from "yjs";
+import { MonacoBinding } from "y-monaco";
+import * as monaco from "monaco-editor";
+import { WebsocketProvider } from "y-websocket";
 
 function EditorPage() {
   const params = useParams();
@@ -15,6 +19,7 @@ function EditorPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [status, setStatus] = useState(0);
   const { resolvedTheme } = useTheme();
+  const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -55,6 +60,38 @@ function EditorPage() {
     );
   }
 
+  const handleEditorDidMount = (
+    editor: monaco.editor.IStandaloneCodeEditor,
+    monaco: any
+  ) => {
+    try {
+      editorRef.current = editor;
+      const doc = new Y.Doc();
+      const model: any = editor.getModel();
+      const type = doc.getText("monaco");
+      model.setValue(code);
+      const wsProvider = new WebsocketProvider(
+        "ws://localhost:6561",
+        "editor-users",
+        doc
+      );
+      wsProvider.on("status", (event: { status: string }) => {
+        if (event.status === "connected") {
+          console.log("Bağlantı başarıyla kuruldu!");
+        }
+      });
+
+      const binding = new MonacoBinding(
+        type,
+        model,
+        new Set([editor]),
+        wsProvider.awareness
+      );
+    } catch (error) {
+      console.error("Error in handleEditorDidMount:", error);
+    }
+  };
+
   return (
     <>
       <div className="flex">
@@ -64,8 +101,8 @@ function EditorPage() {
             key={code}
             height="100%"
             language={detectLanguage(code)}
-            value={code}
             theme={resolvedTheme === "dark" ? "vs-dark" : "light"}
+            onMount={handleEditorDidMount}
           />
         </div>
       </div>
